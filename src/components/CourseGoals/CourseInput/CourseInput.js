5e7fae152911521334053ce679d857b5ca70e94d
Simone <simonesala@me.com>
2022-03-14 12:05:37 +0100

    reset valid

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -7,6 +7,9 @@ const CourseInput = (props) => {
     const [enteredValue, setEnteredValue] = useState("");
     const [isValid, setIsValid] = useState(true);
     const goalInputChangeHandler = (event) => {
+        if (event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
         setEnteredValue(event.target.value);
     };
 
@@ -26,7 +29,10 @@ const CourseInput = (props) => {
                     Course Goal
                 </label>
                 <input
-                    style={{ borderColor: !isValid ? "red" : "black" }}
+                    style={{
+                        borderColor: !isValid ? "red" : "black",
+                        background: !isValid ? "salmon" : "transparent",
+                    }}
                     type="text"
                     onChange={goalInputChangeHandler}
                 />
